b10d6c8ee22b647fec129f4effe478d2b5cbbdcb
lucasch-n <lucascnam@gmail.com>
2024-08-08 12:49:01 -0400

    Added stream and reorganized main code.

--- main.py
+++ main.py
@@ -11,30 +11,42 @@ from langchain_community.vectorstores import Qdrant
 from langchain_community.llms import Ollama
 from openai import OpenAI
 
+def ask_rag(db, query: str, client):
+  docs = db.similarity_search(query)
+  search_results = ''.join([doc.page_content for doc in docs])
 
-query = st.chat_input("Ask a question!")
+  response = client.chat.completions.create(
+    model="llama3",
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant."},
+      {"role": "user", "content": query + search_results}
+    ]
+  )
+
+  with st.chat_message("assistant"):
+      reply = st.write_stream(*response.choices[0].message.content)
+
 
 # Load and chunk documents
+def load_chunks(docs):
+  
+
+
+  chunks = []
+  for doc in docs:
+      raw_document = PyMuPDFLoader(doc).load()
+      text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+      doc_chunks = text_splitter.split_documents(raw_document)
+      for chunk in doc_chunks:
+          chunks.append(chunk) 
+  return chunks
+
+query = st.chat_input("Ask a question!")
 pdfdb = ['./DocLib/7.06__The_Quantum_Harmonic_Oscillator.pdf']
-
-
-chunks = []
-for doc in pdfdb:
-    raw_document = PyMuPDFLoader(doc).load()
-    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    doc_chunks = text_splitter.split_documents(raw_document)
-    for chunk in doc_chunks:
-        chunks.append(chunk) 
-
 # Load vector embedding model
 embeddings = HuggingFaceEmbeddings(model_name="snowflake/snowflake-arctic-embed-m")
-db = Chroma.from_documents(chunks, embeddings)
-docs = db.similarity_search(query)
-search_results = ''.join([doc.page_content for doc in docs])
-# query = "What is a dual space?"
-# text = "This is a test document."
-# query_result = embeddings.embed_query(query)
-# print(query_result[:3])
+db = Chroma.from_documents(load_chunks(pdfdb), embeddings)
+
 
 
 client = OpenAI(
@@ -43,6 +55,9 @@ client = OpenAI(
 )
 
 
+if query is not None:
+    ask_rag(db, query, client)
+
 # r = requests.get('http://localhost:11434/v1/chat/completions', data={
 #  "model": "llama3",
 #  "messages": [
@@ -50,16 +65,11 @@ client = OpenAI(
 #    {"role":"user","content":"what is FRS?"}
 #  ]
 # })
-response = client.chat.completions.create(
-  model="llama3",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": query + search_results}
-  ]
-)
 
-with st.chat_message("assistant"):
-    reply = st.write(response.choices[0].message.content)
 # print(r.text)
+# query = "What is a dual space?"
+# text = "This is a test document."
+# query_result = embeddings.embed_query(query)
+# print(query_result[:3])
 
 #{"model": "llama3", "prompt": [query].append(docs), "stream": False}
